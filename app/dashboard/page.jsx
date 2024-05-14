4d54f0b9168407c02c8a3200ebb7af1b7f8e28e0
SujashB <sjbarman@ucdavis.edu>
2024-05-14 13:11:33 -0700

    Finished Landing Page and Clerk Auth

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button } from 'antd'
+
+const page = () => {
+  return (
+    <div>
+      
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Button type="dashed" className="w-[400px] h-[100px] bg-slate-900 hover:bg-slate-600 text-white text-xl">
+            +
+        </Button>
+      </div>
+      
+      
+
+    </div>
+  )
+}
+
+export default page
